4636bc039131e64f2fb02b2e59bd3f741697a899
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-09-14 17:08:44 -0300

    feat: Implementando a função saveCartItems e criando testes necessários
     #ref: requisito 6

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,5 +1,6 @@
-const saveCartItems = () => {
-  // seu código aqui
+const saveCartItems = (cartItem) => {
+  const local = localStorage.setItem('cartItem', cartItem); 
+  return local;
 };
 
 if (typeof module !== 'undefined') {
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,21 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('1.1 Verifique se a função saveCartItems com argumento "cartItem",o localStorage.setItem é chamado', () => {
+    saveCartItems('MLB1341706310')
+    expect(localStorage.setItem).toHaveBeenCalled()
+  });
+   
+  it('1.2 Verifique se a função saveCartItems com argumento "cartItem",o localStorage.setItem é chamado dois parametros', () => {
+    const cartItem = 'MLB1341706310'
+    saveCartItems(cartItem)
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItem',cartItem)
+  });
+   
+  // it('1.1 Verifique se ', () => {
+  //   saveCartItems('MLB1341706310')
+  //   expect(localStorage.setItem).toHaveBeenCalled()
+  // });
+   
+   
 });
